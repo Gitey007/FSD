b308d46999d37b332ae9c0090edfb8d615b5b540
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-07-28 09:51:17 +0530

    add initial JavaScript code with commented functions and IIFE

---
+++ index.js
@@ -0,0 +1,28 @@
+// // console.log("hello I am  missing js")
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// a = sum(9,6);
+// console.log( "sum is : ",a);
+// =========================================
+// function root(a){
+//     return Math.sqrt(a);
+// }
+
+// function sum1(a,b){
+//     return root(a)+root(b);
+// }
+
+// b = sum1(9,6);
+// console.log( "sum1 is : ",b);
+
+// const sum=(a,b)=>{return a+b};
+//  a = sum(9,6);
+// console.log( "sum is : ",a);
+// =========================================
+// (()=>{
+//     console.log("hey ..... using IIFE");
+// })();
+
