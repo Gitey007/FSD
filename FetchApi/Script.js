6bd4d47712b3812c23808182d12e5373280edb19
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-09-01 10:06:46 +0530

    implement async data fetching and display in table format

--- FetchApi/Script.js
+++ FetchApi/Script.js
@@ -1,7 +1,34 @@
 const container = document.getElementById("container");
 const btn = document.getElementById("btn");
+const disp = document.getElementById("display");
+const load = document.getElementById("loader");
 
-function fetch(){
-    alert("hiii");
+async function fetch1() {
+  // alert("hiii");
+  try {
+    loader.innerHTML = '<h2>loading data .....</h2>';
+    const serverData = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await serverData.json();
+    // console.log(jsonData[0].title);
+    // disp.innerHTML = `${jsonData[0].title}`;
+    let table = `<table border = 2px>
+    ${
+        jsonData.map((ele)=>(
+    `<tr>
+    <td><img src =" ${ele.image}" height = 200 width = 200> </img></td>
+    <td>${ele.title}</td>
+    <td>${ele.description}</td>
+    <td>${ele.price}</td>
+    <td>${ele.category}</td>
+    </tr>`
+        ))
+    }
+    </table>`
+    disp.innerHTML=table;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    loader.innerHTML = '';
+  }
 }
-btn.addEventListener('click',fetch);
+btn.addEventListener("click", fetch1);
