f1c7c5b90f14d10febc4b807b6cce2f1982ebce6
SAHUL KUMAR <sahulshaw92@gmail.com>
2026-09-01 08:55:59 +0530

    add HTML and JavaScript files for basic DOM manipulation and event handling

---
+++ FetchApi/Script.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("container");
+const btn = document.getElementById("btn");
+
+function fetch(){
+    alert("hiii");
+}
+btn.addEventListener('click',fetch);
